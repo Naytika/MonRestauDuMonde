3fb64855d52f7fd1e1f8b3f2e2e962a67a5f1743
Aude <audechevalie@gmail.com>
2018-09-28 13:10:51 +0200

    tabbarcustom

---
+++ MonRestauDuMonde/Controller/MonTabBar.swift
@@ -0,0 +1,22 @@
+//
+//  MonTabBar.swift
+//  MonRestauDuMonde
+//
+//  Created by CHEVALIER on 28/09/2018.
+//  Copyright © 2018 CHEVALIER. All rights reserved.
+//
+
+import UIKit
+
+class MonTabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tabBar.barTintColor = GRIS_TRES_FONCE
+        tabBar.tintColor = GRIS_TRES_CLAIR
+        
+        
+    }
+
+}
---
+++ MonRestauDuMonde/Utilitaire/Constantes.swift
@@ -0,0 +1,15 @@
+//
+//  Constantes.swift
+//  MonRestauDuMonde
+//
+//  Created by CHEVALIER on 28/09/2018.
+//  Copyright © 2018 CHEVALIER. All rights reserved.
+//
+
+import UIKit
+
+//creation de couleur
+let NOIR = UIColor.black
+// Noir c'est 0 / 255 et blanc 255/255
+let GRIS_TRES_FONCE = UIColor(red: 30/255, green: 30/255, blue: 30/255, alpha: 1)
+let GRIS_TRES_CLAIR = UIColor(red: 240/255, green: 240/255, blue: 240/255, alpha: 1)
